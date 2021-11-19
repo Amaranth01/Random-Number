3f68e21b91037c1b480d6a2af34a03110c18646d
Amaranth01 <vanessa.nectoux@gmail.com>
2021-11-19 10:06:53 +0100

    Project 'Find my Number'

---
+++ script.js
@@ -0,0 +1,75 @@
+let randomNumber = Math.floor(Math.random()*100) + 1;
+
+let guesses = document.querySelector('.guesses');
+let lastResult = document.querySelector('.lastResult');
+let lowOrHi = document.querySelector('.lowOrHi');
+
+let guessSubmit = document.querySelector('.guessSubmit');
+let guessField = document.querySelector('.guessField');
+
+let guessCount = 1;
+let resetButton;
+guessField.focus();
+
+function checkGuess () {
+    let userGuess = Number(guessField.value);
+    if (guessCount === 1) {
+        guesses.textContent = 'Propositions précédentes : ';
+    }
+    guesses.textContent += userGuess + ' ';
+
+    if (userGuess === randomNumber) {
+        lastResult.textContent = 'Bravo, vous avez trouvé le bon chiffre !';
+        lastResult.style.backgroundColor = 'darkgreen';
+        lowOrHi.textContent = ' ';
+
+        gameOver();
+    }
+    else if (guessCount===10) {
+        lastResult.textContent = 'Vous avez perdu';
+        gameOver;
+    }
+    else {
+        lastResult.textContent = 'Faux !';
+        lastResult.style.backgroundColor = 'red';
+        if (userGuess < randomNumber) {
+            lowOrHi.textContent = ' Le nombre saisi est trop petit';
+        }
+        else if (userGuess > randomNumber) {
+            lowOrHi.textContent = 'Le nombre saisi est trop grand';
+        }
+    }
+
+    guessCount++;
+    guessField.value = '';
+    guessField.focus();
+}
+
+guessSubmit.addEventListener('click', checkGuess);
+
+function gameOver() {
+    guessField.disabled = true;
+    guessSubmit.disabled = true;
+    resetButton = document.createElement('button');
+    resetButton.textContent = 'Start new game';
+    document.body.appendChild(resetButton);
+    resetButton.addEventListener('click', resetGame);
+}
+
+function resetGame () {
+    guessCount = 1;
+
+    let resetParas = document.querySelectorAll('.resultParas');
+    for (let i = 0; i < resetParas.length; i++) {
+        resetParas[i].textContent = ' ';
+    }
+    resetButton.parentNode.removeChild(resetButton);
+    guessField.disable = false;
+    guessSubmit.disabled = false;
+    guessField.value = ' ';
+    guessField.focus();
+
+    lastResult.style.backgroundColor = 'aqua';
+
+    randomNumber = Math.floor(Math.random () * 100) +1;
+}
